396103d14a1922a1552d67a09036cc86ef87bd4d
Shogunism <k23067kk@aitech.ac.jp>
2024-11-28 15:18:34 +0900

    uoooo

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from diaries.DiarySample import DiarySample
 
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ] 
+diaries = [DiarySample(), 
+           SuzukiDiary(),
+           ] 
 
 for d in diaries:
     print("---------------------------------")
